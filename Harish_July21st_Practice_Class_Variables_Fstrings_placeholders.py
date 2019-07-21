6662d283401b5ceaf1486128f08c9bc0c7d33953
hari2pega <53147387+hari2pega@users.noreply.github.com>
2019-07-21 12:04:41 -0400

    Harish_July21st_Practice_Class_Variables_Fstrings_placeholders Sheet
    Harish_July21st_Practice_Class_Variables_Fstrings_placeholders

---
+++ Harish_July21st_Practice_Class_Variables_Fstrings_placeholders.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import sys
+
+
+# In[3]:
+
+
+print("Hello World!")
+
+
+# In[5]:
+
+
+#introduction to Variables .....
+
+X=10
+Y=5
+X+Y 
+
+
+# In[7]:
+
+
+message = "Hello Python World!"
+
+print(message)
+
+
+# In[ ]:
+
+
+#Introduction Data Types in Python
+
+#1. strings...
+
+
+# In[8]:
+
+
+# Introduction to strings.....
+
+# -- A String is a series of charecters
+# A string is an immutable data type
+# How to define a String ?
+# Anything inside a string is considered as string ....
+
+name = "tony stark"
+
+print(name)
+
+
+
+
+# In[11]:
+
+
+first_name = "Kumar"
+last_name = "Harish"
+print(first_name)
+#1. Can we declare or assign a space while giving the variable name ? Ans: No .. we can give unsderscore to achieve this
+
+
+# In[16]:
+
+
+name ="tony STARK"
+#print(name.title())
+#print(name.lower())
+print(name.upper())
+
+#1. How do u change the Input to titlecase, uppercase or lowercase?
+
+
+# In[18]:
+
+
+#Using variables in the strings
+
+first_name = "Tony"
+last_name = "stark"
+#F- Strings .... where in we can able to assign references to other variables as well -- It supports to 3.6 or Above version
+# F string means -- Formatted Strings
+full_name= f"{first_name} {last_name}"
+#print(full_name)
+
+print(full_name.title())
+
+
+
+# In[19]:
+
+
+#Adding whitespaces to strings with tabs and new lines .......
+
+#Any non printing charecter is whitespace ..... !!! 
+# \t -- Adding additional 5/4 spaces (Tab space) before the charecters ....
+
+
+print("python")
+
+
+# In[20]:
+
+
+print("python ")
+
+
+# In[21]:
+
+
+print('\tpython')
+
+# Methods of declaring a string... 
+# double quote, single quote and triple quote Strings can be 
+
+
+# In[ ]:
+
+
+declare ="something"
+declare1 ='something1'
+declare2 = " 'something2'"
+declare3 ="""something4"""
+example = "john's dad name is unique"
+# -- IQ # How do u include a afostophe between the strings ...!!!
+# -- 
+print("")
+
+
+# In[24]:
+
+
+# -- How to print the output in the new line ..!!
+print("Languages:\npython\nC\nJava\n.Net\nC++\nswift")
+
+
+# In[30]:
+
+
+#Stripping/Eliminating the whitespaces..!!
+fav_language =" Python "
+#print(fav_language)
+#print(fav_language.rstrip())
+#print(fav_language.lstrip())
+print(fav_language.strip())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
