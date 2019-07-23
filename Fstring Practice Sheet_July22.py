cae510c21da8e384bc7b58dca6145f46d7a884ed
hari2pega <53147387+hari2pega@users.noreply.github.com>
2019-07-22 23:33:25 -0400

    Fstring Practice Sheet_July22
    Fstring and Concatenation Practice Sheet_July22

---
+++ Fstring Practice Sheet_July22.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+first_name = "Harish"
+last_name= "kumar"
+#print(f"Hello, Welcome to Python World {first_name} {last_name}!!")
+print( 'Hello , Welcome to Python world' + ' ' +(first_name) + ' ' +(last_name))
+      
+
+
+# In[13]:
+
+
+first_name = "Harish"
+last_name= "kumar"
+print(f"Hello, Welcome to Python World {first_name} {last_name}!!")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
