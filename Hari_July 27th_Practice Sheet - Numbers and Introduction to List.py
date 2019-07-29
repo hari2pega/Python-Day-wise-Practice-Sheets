31ba9c3b592a973704fcc524fc70eb3ac667a9a3
hari2pega <53147387+hari2pega@users.noreply.github.com>
2019-07-29 15:47:26 +0530

    Hari_July 27th_Practice Sheet - Numbers and Introduction to List
    Hari Kumar - Practice sheet for July 27th weekend class#3

---
+++ Hari_July 27th_Practice Sheet - Numbers and Introduction to List.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Commenting the Line
+#What ever has been written after the Hash symbol, It will be considered as Comment
+#Numbers
+#Integers 
+2+3
+
+
+# In[2]:
+
+
+#Numbers
+3-2
+
+
+# In[3]:
+
+
+#Float - It will be give the decimal value - Declaring in Decimal is called Float 
+0.1+0.2
+
+
+# In[8]:
+
+
+# Assigning Numbers
+X=2
+Y=8
+Z=4
+X+Y+Z
+
+
+# In[9]:
+
+
+#Advance approach of aasignment
+X,Y,Z = 9,4,6
+X+Y+Z
+
+
+# In[10]:
+
+
+#Constants in Python: A constant in a Python is like a variable whose value says the same approach --  
+# -- through out the life of the program
+# In python we will be declaring the constant variables in Capital Letters or Capital case
+MAX_CONNECTION = 100
+
+
+# In[13]:
+
+
+#*** Introduction to List - Data type 
+# 1. List is an Mutable data type -- All modifications done on this
+# 2. String is a Immutable data type
+# 3. Alist is a collection of items in a particular order.
+# 4. How to define a List - [] - Separated by Commas (,)
+# Eg: Bicycles = ['Hero','Redline', 'Atlas', 'Trek', 'Ranger']
+Bicycles = ['Hero','Redline','Atlas','Trek','Ranger']
+print(Bicycles)
+
+
+# In[19]:
+
+
+# How to print a Individual element from a list?
+# Ans: With the help of indexing
+# **** From where Index will start -- Point 0 -- But not 1 **** 
+# Want to print first element from the List?
+Bicycles = ['Hero','Redline','Atlas','Trek','Ranger']
+print (Bicycles[0])
+
+
+# In[20]:
+
+
+Bicycles = ['Hero','Redline','Atlas','Trek','Ranger']
+print (Bicycles[3])
+
+
+# In[21]:
+
+
+Bicycles = ['Hero','Redline','Atlas','trek','Ranger']
+print (Bicycles[3].title())
+
+
+# In[22]:
+
+
+Bicycles = ['Hero','Redline','Atlas','Trek','ranger']
+print (Bicycles[4].upper())
+
+
+# In[23]:
+
+
+Bicycles = ['Hero','Redline','Atlas','Trek','Ranger']
+print (Bicycles[4].lower())
+
+
+# In[24]:
+
+
+Bicycles = ['Hero','Redline','Atlas','Trek','Ranger']
+print (Bicycles[2])
+
+
+# In[ ]:
+
+
+
+
