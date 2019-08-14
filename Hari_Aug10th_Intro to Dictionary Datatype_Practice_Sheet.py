3a319605a7807f08e2bd2268078ace7e6d16f016
hari2pega <53147387+hari2pega@users.noreply.github.com>
2019-08-14 06:32:14 -0400

    Hari_Aug10th_Introduction to Dictionary Datatype_Practice_Sheet
    Hari_Aug10th_2019_Introduction to Dictionary Datatype_Practice_Sheet

---
+++ Hari_Aug10th_Intro to Dictionary Datatype_Practice_Sheet.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#introduction to dictionary datatype
+
+
+# In[ ]:
+
+
+# A Dictionary datattype in python is a collection of key-value pairs
+
+# Each key is connected with a value 
+
+# Dictionary is a mutable data type - It can be able to modify, add and delete after the assignment of the values as well
+
+# By using the flower brackets ... {}
+
+
+# In[1]:
+
+
+alien_0 = {'color':'green','points':5}
+print(alien_0)
+
+
+# In[2]:
+
+
+alien_0 = {'color':'green','points':5}
+print(alien_0['color'])
+
+
+# In[3]:
+
+
+alien_0 = {'color':'green','points':5}
+print(alien_0['points'])
+
+
+# In[10]:
+
+
+#How to initialize a empty dictionary
+alien_1 = {}
+alien_1['color'] = 'yellow'
+alien_1['points'] = 10
+alien_1['score'] = 60
+alien_1['net total'] = 100
+
+print(alien_1)
+
+print(f"the alien is in {alien_1['color']}")
+alien_1['color'] = 'blue'
+print(f"the alien is in {alien_1['color']}")
+
+
+# In[ ]:
+
+
+#using get to access the values.
+# -- when ever there is no specific key in the given dictionary , but we try to access that key , then get() will be used to print out the message !!!
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
